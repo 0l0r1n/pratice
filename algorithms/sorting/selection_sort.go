04387f9637841be2e1bdb68c55cbde0779c63a4c
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-10-08 19:07:11 +0200

    feat: renamed folder and add selection sort

--- algorithms/sorting/selection_sort.go
+++ algorithms/sorting/selection_sort.go
@@ -4,6 +4,12 @@ package sorting
 func SelectionSort(nums []int) {
 	n := len(nums)
 	for i := 1; i < n; i++ {
-
+		min := i
+		for j := i + 1; j < n; j++ {
+			if nums[j] < nums[min] {
+				min = j
+			}
+			nums[j], nums[min] = nums[min], nums[j]
+		}
 	}
 }
