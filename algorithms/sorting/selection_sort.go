b1354b8892b968afeb633b6475047bfe315222d4
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-10-08 19:16:47 +0200

    fix: fix typo

--- algorithms/sorting/selection_sort.go
+++ algorithms/sorting/selection_sort.go
@@ -3,13 +3,13 @@ package sorting
 // SelectionSort -> https://en.wikipedia.org/wiki/Selection_sort
 func SelectionSort(nums []int) {
 	n := len(nums)
-	for i := 1; i < n; i++ {
+	for i := 0; i < n; i++ {
 		min := i
 		for j := i + 1; j < n; j++ {
 			if nums[j] < nums[min] {
 				min = j
 			}
-			nums[j], nums[min] = nums[min], nums[j]
 		}
+		nums[i], nums[min] = nums[min], nums[i]
 	}
 }
