469b79aac9fd04891493fdff9c2ab7c28a16297b
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-10-30 09:59:51 +0100

    notes on chapter 1 of algorithms 4th edition + some code

---
+++ algorithms/chapter 1/BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+    
+    public int indexOf(int[] a, int key) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = (high - low)/2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        // read the integers from a file
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+
+        // sort the array
+        Arrays.sort(whitelist);
+
+        // read integer key from standard input; print if not in whitelist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(whitelist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
--- algorithms/week 1/code/IUF.java
+++ algorithms/week 1/code/IUF.java
@@ -3,5 +3,5 @@ public interface IUF {
     void union(int p, int q); // adds connection between p and q
     boolean connected(int p, int q); // are p and q in the same component?
     int find(int p); // component identifier for p
-    int count(); // number of components
+    int count(); // number of components 
 }
