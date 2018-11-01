11844c61eef2bf48c5892e83b1bc122a34dda8f8
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-01 21:16:39 +0100

    update

--- algorithms/chapter 1/code/BinarySearch.java
+++ algorithms/chapter 1/code/BinarySearch.java
@@ -1,5 +1,7 @@
 
 import java.util.Arrays;
+import java.util.ArrayList;
+
 
 /**
  *  The {@code BinarySearch} class provides a static method for binary
@@ -42,28 +44,46 @@ public class BinarySearch {
         return -1;
     }
 
-    /**
-     * Returns the index of the specified key in the specified array.
-     * This function is poorly named because it does not give the <em>rank</em>
-     * if the array has duplicate keys or if the key is not in the array.
-     *
-     * @param  key the search key
-     * @param  a the array of integers, must be sorted in ascending order
-     * @return index of key in array {@code a} if present; {@code -1} otherwise
-     * @deprecated Replaced by {@link #indexOf(int[], int)}.
-     */
-    @Deprecated
-    public static int rank(int key, int[] a) {
+    private static int[] removeDuplicates(int[] a) {
+        ArrayList<Integer> intlist = new ArrayList<Integer>();
+
+        intlist.add(a[0]);
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] != a[i - 1]) {
+                intlist.add(a[i]);
+            }
+        }
+
+        int size = intlist.size();
+        int[] a1 = new int[size];
+
+        for (int i = 0; i < a1.length; i++) {
+            a1[i] = intlist.get(i);
+        }
+
+        return a1;
+    }
+
+    private static int rank(int[]a, int key) {
+        BinarySearch.removeDuplicates(a);
         return indexOf(a, key);
     }
 
-    /**
-     * Reads in a sequence of integers from the whitelist file, specified as
-     * a command-line argument; reads in integers from standard input;
-     * prints to standard output those integers that do <em>not</em> appear in the file.
-     *
-     * @param args the command-line arguments
-     */
+    private static int count(int[] a, int key) {
+        int counter = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == key && i + 1 != a.length) {
+                counter++;
+                i++;
+                while (a[i] == key && i + 1 != a.length) {
+                    counter++;
+                    i++;
+                }
+            }
+        }
+        return counter;
+    }
+
     public static void main(String[] args) {
 
         // read the integers from a file
@@ -72,10 +92,12 @@ public class BinarySearch {
 
         // sort the array
         Arrays.sort(whitelist);
-
+        
         // read integer key from standard input; print if not in whitelist
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
+            StdOut.println("count: " + BinarySearch.count(whitelist, key));
+            StdOut.println("rank: " + BinarySearch.rank(whitelist, key));
             if (BinarySearch.indexOf(whitelist, key) == -1)
                 StdOut.println(key);
         }
