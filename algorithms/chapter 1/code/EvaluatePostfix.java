46ab4b8dbab7b8d2da709842e29bb69399a70666
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-12 06:36:49 +0100

    add evaluate postfix

--- algorithms/chapter 1/code/EvaluatePostfix.java
+++ algorithms/chapter 1/code/EvaluatePostfix.java
@@ -2,42 +2,36 @@ import edu.princeton.cs.algs4.*;
 
 public class EvaluatePostfix {
 
-    private static int evaluate(String s) {
+    public static void main(String[] args) {
+        StackLinkedList<Integer> stack = new StackLinkedList<Integer>();
         int result = 0;
-        StackLinkedList<Character> stack = new StackLinkedList<Character>();
-        for (int index = 0; index < s.length(); index++) {
-            StdOut.println(ch);
-            char ch = s.charAt(index);
-            if (Character.isDigit(ch)) stack.push(ch);
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+        
+            if (Character.isDigit(s.charAt(0))) stack.push(Integer.parseInt(s));
             else {
-                while (!stack.isEmpty()){
-                    int temp = stack.pop();
-                    switch(ch) {
-                        case '+':
-                            result+=temp;
+                while (!stack.isEmpty()) {
+                    int nextInt = stack.pop();
+                    switch(s) {
+                        case "+":
+                            result += nextInt;
                             break;
-                        case '-':
-                            result-=temp;
+                        case "-":
+                            result -= nextInt;
                             break;
-                        case '*':
-                            result*=temp;
+                        case "*":
+                            result *= nextInt;
                             break;
-                        case '/':
-                            result/=temp;
+                        case "/":
+                            result /= nextInt;
                             break;
+                        default:
+                            throw new UnsupportedOperationException("That is not a character that we expected!");
                     }
                 }
             }
+            StdOut.println("Result: " + result);
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        while (!StdIn.isEmpty()) {
-            String s = StdIn.readString();
-            StdOut.println("Result: " + evaluate(s));
-        }
-        StdOut.println();
     }
 
 }
