247a84fb2ccd335498f288c24023051eff1c9f50
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-13 09:54:29 +0100

    exercises on linked lists

--- algorithms/chapter 1/code/FindInLinkedList.java
+++ algorithms/chapter 1/code/FindInLinkedList.java
@@ -1,11 +1,9 @@
 public class FindInLinkedList {
 
-    private static boolean find(Node<String> node, String key) {
+    public static boolean find(Node<String> node, String key) {
         if (node == null) return false;
         if (node.item == key) return true;
         return find(node, key);
     }
-    public static void main(String[] args) {
-        find(null, null);
-    }
+
 }
