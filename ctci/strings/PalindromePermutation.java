6f4847248fc05ac8f10d08428eda5214638d89c0
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-19 20:31:33 +0100

    better solution to palindrome problem

--- ctci/strings/PalindromePermutation.java
+++ ctci/strings/PalindromePermutation.java
@@ -22,4 +22,37 @@ public class PalindromePermutation {
         return true;
     }
 
+    public static boolean isPermutationOfPalindrome(String s) {
+        int[] table = buildCharFrequencyTable(s);
+        return checkMaxOneOdd(table);
+    }
+
+    private static boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        for (int count : table) {
+            if (count % 2 == 1) return false;
+            foundOdd = true;
+        }
+        return foundOdd;
+    }
+
+    private static int getCharNumber(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if (a <= val && val <= z) {
+            return val - a;
+        }
+        return -1;
+    }
+
+    private static int[] buildCharFrequencyTable(String phrase) {
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for (char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if ( x != -1) table[x]++;
+        } 
+        return table;
+    }
+
 }
---
+++ other/spring.md
@@ -0,0 +1,5 @@
+# Spring Framework
+
+## Important annotations
+
+- @SpringBootApplication: adds @Configuration, @EnableAutoConfiguration and @ComponentScan
