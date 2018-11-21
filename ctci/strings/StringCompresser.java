d29a60b73263473cde7ee39640cc42a202ed7e02
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-21 19:17:15 +0100

    fix to string compression

--- ctci/strings/StringCompresser.java
+++ ctci/strings/StringCompresser.java
@@ -3,9 +3,10 @@ import java.util.HashMap;
 class StringCompresser {
 
     public static String compress(String s) {
+        int length = s.length();
         StringBuilder buffer = new StringBuilder();
         HashMap<Character, Integer> occurrences = new HashMap();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < length; i++) {
             char ch = s.charAt(i);
             if (occurrences.contains(ch)) occurrences.put(ch, occurrences.get(ch) + 1);
             else occurrences.put(ch, 1);
@@ -14,6 +15,6 @@ class StringCompresser {
             buffer.append(c + occurrences.get(c));
         }
         String newString = buffer.toString();
-        return newString.length() > s.length() ? newString : s;
+        return newString.length() < length ? newString : s;
     }
 }
