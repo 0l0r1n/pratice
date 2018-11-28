e6be5cba38c47db57bc186a89e3d38aea1e3e0f3
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-28 06:13:54 +0100

    Find middle node with cursor

---
+++ ctci/linked-lists/MiddleNode.java
@@ -0,0 +1,13 @@
+class MiddleNode { 
+
+    public Node find(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
