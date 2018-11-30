feb771b136af5a243c7c66ce8737794771fa9904
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-11-30 06:39:49 +0100

    yet another way to find intersection node

--- ctci/linked-lists/IntersectionNode.java
+++ ctci/linked-lists/IntersectionNode.java
@@ -24,4 +24,53 @@ class IntersectionNode {
         return (pA == pB && pA != null) ? pA : null;
     }
 
+    ListNode findPointersV2(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) return null;
+        Result result1 = getTailAndSize(list1);
+        Result result2 = getTailAndSize(list2);
+        if(result1.tail != result2 != tail) return null;
+
+        ListNode shorter = result1.size < result2.size ? list1 : list2;
+        ListNode longer = result1.size < result2.size ? list2 : list1;
+
+        longer = getKthNode(longer, Math.abs(result.size - result2.size));
+
+        while (shoter != longer) {
+            shoter = shoter.next;
+            longer = longer.next;
+        }
+        return longer;
+    }
+
+    class Result {
+        ListNode tail;
+        int size;
+
+        Result(ListNode tail, int size) {
+            this.tail = tail;
+            this.size = size;
+        }
+        
+    }
+
+    Result getTailAndSize(ListNode list) {
+        if (list == null) return null;
+        int size = 1;
+        ListNode current = list;
+        while (current.next != null) {
+            size++;
+            current = current.next;
+        }
+        return new Result(current, size);
+    }
+
+    ListNode getKthNode(ListNode head, int k) {
+        ListNode current = head;
+        while (k > 0 && current != null) {
+            current = current.next;
+            k--;
+        }
+        return current;
+    }
+
 }
