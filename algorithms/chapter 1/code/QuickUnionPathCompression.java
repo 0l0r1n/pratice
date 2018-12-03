4c6f7130147d36fadbfc50e7148a236e8836bcf8
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-12-03 19:36:36 +0100

    more exercises on union find

---
+++ algorithms/chapter 1/code/QuickUnionPathCompression.java
@@ -0,0 +1,61 @@
+import edu.princeton.cs.algs4.*;
+
+
+public class QuickUnionPathCompressionUF {
+    
+    private int[] id;    
+    private int count;   
+
+    public QuickUnionPathCompressionUF(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+  
+   
+    public int find(int p) {
+        int root = p;
+        while (root != id[root])
+            root = id[root];
+        while (p != root) {
+            int newp = id[p];
+            id[p] = root;
+            p = newp;
+        }
+        return root;
+    }
+
+    
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        id[rootP] = rootQ;
+        count--;
+    }
+
+    
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        QuickUnionPathCompressionUF uf = new QuickUnionPathCompressionUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+}
