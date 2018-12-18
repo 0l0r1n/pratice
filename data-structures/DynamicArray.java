fb8837a203e7bb614db67cf4b8a819e171c605a5
Leo Alves <leonardo.filipe.alves@gmail.com>
2018-12-18 11:13:21 -0200

    make dynamic array implemementation more dynamic

--- data-structures/DynamicArray.java
+++ data-structures/DynamicArray.java
@@ -3,21 +3,19 @@ class DynamicArray {
     int[] arr;
     int capacity;
     int size;
-    int count;
-    int first;
-    int last;
-
+    
     DynamicArray(int capacity) {
+        this.capacity = capacity;
         arr = new int[capacity];
         size = 0;
     }
 
     void add(int val) {
-        if (count == arr.length) {
-            int[] temp = new int[arr.length * 2];
-            for (int i = 0; i < arr.length; i++) temp[i] = arr[i];
+        if (size == capacity) {
+            capacity *= 2;
+            int[] temp = new int[capacity];
+            for (int i = 0; i < capacity; i++) temp[i] = arr[i];
             arr = temp;
-            size = arr.length;
         }
         arr[size] = temp;
         size++;
