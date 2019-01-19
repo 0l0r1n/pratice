09075da136c8439c70ddb71386d0458be9c9dc07
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-01-19 13:38:14 +0100

    new data structs

---
+++ ctci/stacks and queues/MyStack.java
@@ -0,0 +1,22 @@
+import java.util.EmptyStackException;
+
+public class MyStack<T> {
+
+    private StackNode<T> top;
+
+    public T pop() {
+        if (top == null) throw new EmptyStackException();
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    private static class StackNode<T> {
+        private T data;
+        private StackNode<T> next;
+
+        public StackNode(T data) {
+            this.data = data;
+        }
+    }
+}
