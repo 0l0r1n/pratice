48b06773feca50c3c6053a80fd0d6b96d68e2e0b
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-01-21 20:24:21 +0100

    cadd randomness to quick sort

--- algorithms (implementation)/QuickSort.java
+++ algorithms (implementation)/QuickSort.java
@@ -1,8 +1,35 @@
+import java.util.Random;
+
 public class QuickSort {
 
     public static void sort(int[] arr) { 
         sort(arr, 0, arr.length - 1);
     } 
+
+    public static void sortRandom(int[] arr) {
+        sortRandom(arr, lo, hi);
+    }
+
+    private static void sortRandom(int[] arr, int lo, int hi) {
+        if (lo >= hi) return;
+        int pivot = new Random(arr.length).nextInt();
+        int index = partition(arr, lo, hi, pivot);
+        sortRandom(arr, lo, index -1);
+        sortRandom(arr, index, hi);
+    }
+
+    private static int partition(int[] arr, int lo, int hi, int pivot) {
+        while (lo <= hi) {
+            while (arr[lo] < arr[pivot]) lo++;
+            while (arr[hi] > arr[pivot]) hi--;
+        }
+        if (lo < hi) {
+            swap(arr, lo, hi);
+            lo++;
+            hi--; 
+        }
+        return left;
+    }
 
     private static void sort(int[] arr, int lo, int hi) {
         if (lo < hi) {
