234b2de0242fd0dc23fd65edeb2b71dc2f19ad85
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-01-28 20:05:56 +0100

    fix typos

--- algorithms (implementation)/Dijkstra.java
+++ algorithms (implementation)/Dijkstra.java
@@ -11,8 +11,7 @@ class Dijkstra {
         while (unsettledNodes.size() != 0) {
             Node currentNode = getLowestDistanceNode(unsettledNodes);
             unsettledNodes.remove(currentNode);
-            for (Entry < Node, Integer> adjacencyPair: 
-              currentNode.adjacentNodes.entrySet()) {
+            for (Entry <Node, Integer> adjacencyPair : currentNode.adjacentNodes.entrySet()) {
                 Node adjacentNode = adjacencyPair.getKey();
                 Integer edgeWeight = adjacencyPair.getValue();
                 if (!settledNodes.contains(adjacentNode)) {
@@ -38,13 +37,13 @@ class Dijkstra {
         return lowestDistanceNode;
     }
 
-    private static void CalculateMinimumDistance(Node evaluationNode, Integer edgeWeigh, Node sourceNode) {
+    private static void calculateMinimumDistance(Node evaluationNode, Integer edgeWeigh, Node sourceNode) {
     Integer sourceDistance = sourceNode.getDistance();
     if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
         evaluationNode.distance = sourceDistance + edgeWeigh;
-        LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+        LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.shortestPath);
         shortestPath.add(sourceNode);
-        evaluationNode.setShortestPath(shortestPath);
+        evaluationNode.shortestPath = shortestPath;
     }
 }
 
