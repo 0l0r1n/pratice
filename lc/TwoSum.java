4268822f87c14558f60df96f0fdf1f73bb10680e
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-03-01 07:01:58 +0100

    add solution to two sum

---
+++ lc/TwoSum.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+class TwoSum {
+
+    private int[] solve(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> complement = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (complement.containsKey(target - nums[i])) {
+                result[0] = complement.get(target - nums[i]);
+                result[1] = i;
+            } else {
+                complement.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
