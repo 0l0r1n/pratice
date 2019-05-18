77af5bbb9fe1504719e545f85d95ff9e8f1595d2
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-05-18 19:10:52 +0200

    chore: add path sum exercise description

--- lc/PathSum.java
+++ lc/PathSum.java
@@ -1,3 +1,8 @@
+/*
+    Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+    Note: A leaf is a node with no children.
+*/
 class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
