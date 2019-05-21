89d132d100333c3066588ecb24c65e4b5a6105c2
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-05-21 20:45:25 +0200

    feat: add palindrome permutation solution

---
+++ books/ctci/arrays & strings/PalindromePermutation.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+/*
+    A palindrome is a word or phrase that is the same forwards or backwards.
+    Characteristics of a palindrome = even chars of each + allows one odd char 
+*/
+public class PalindromePermutation {
+    
+    public static boolean solveWithHashTable(String s) {
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (frequency.containsKey(ch)) frequency.put(ch, frequency.get(ch) + 1);
+            else frequency.put(ch, 1);
+        }
+        boolean oddFound = false;
+        for (Integer value : frequency.values()) {
+            if (value % 2 == 1) {
+                if (oddFound) return false;
+                oddFound = true;
+            }
+        }
+        return true;
+    }
+
+    // space is O(1). Time remains O(n) where n is the length of the string
+    public static boolean solveWithBitVector(String s) {
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(solveWithHashTable("tactcoa"));
+        System.out.println(solveWithHashTable("aabbaa"));
+        System.out.println(solveWithHashTable("tactcoapapa"));
+        System.out.println(solveWithHashTable("dhakweqasdn"));
+    }
+}
