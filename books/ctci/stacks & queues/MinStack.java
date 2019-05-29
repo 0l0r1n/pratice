1ea7bc33e92ebb62da3c654d6ed90ecc038da9eb
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-05-29 06:25:32 +0200

    feat: add min stack

---
+++ books/ctci/stacks & queues/MinStack.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class MinStack extends Stack<Integer> {
+
+    Stack<Integer> min;
+
+    public MinStack() {
+        minStack = new Stack<>();
+    }
+
+    public void push(int value) {
+        if (getMin() > value) {
+            min.push(value);
+        }
+        super.push(value);
+    }
+
+    public Integer pop() {
+        int value = super.pop();
+        if (value == min()) {
+            s2.pop();
+        }
+        return value;
+    }
+
+    private Integer getMin() {
+        if (min.isEmpty()) {
+            return Integer.MAX_VALUE;
+        }
+        return min.peek();
+    }
+}
