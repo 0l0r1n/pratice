97c25de81e03f3de67af11042333879084d98941
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-05-29 05:51:33 +0200

    feat: add solution to findStartOfCycleNode

--- books/ctci/linked lists/LoopDetection.java
+++ books/ctci/linked lists/LoopDetection.java
@@ -13,4 +13,25 @@ public class LoopDetection {
         }
         return false;
     }
+    
+    public static ListNode findStartOfCycleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (slow != null & fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        
+        if (fast == null || fast.next == null) return null;
+        
+        fast = head;
+        
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
 }
