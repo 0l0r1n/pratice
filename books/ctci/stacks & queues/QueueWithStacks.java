a4f5de05f50c0be60750c409315fe034fa586282
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-06-05 15:54:12 +0200

    feat: add solution to queue with stacks

--- books/ctci/stacks & queues/QueueWithStacks.java
+++ books/ctci/stacks & queues/QueueWithStacks.java
@@ -1,3 +1,37 @@
-public class QueueWithStacks {
+import java.util.Stack;
+
+public class QueueWithStacks<T> {
     
+    Stack<T> stackNewest, stackOldest;
+
+    public QueueWithStacks() {
+        stackNewest = new Stack<T>();
+        stackOldest = new Stack<T>();
+    }
+
+    public int size() {
+        return stackNewest.size() + stackOldest.size();
+    }
+
+    public void add(T value) {
+        stackNewest.push(value);
+    }
+
+    public T peek() {
+        shiftStacks();
+        return stackOldest.peek();
+    }
+
+    public T remove() {
+        shiftStacks();
+        return stackOldest.pop();
+    }
+
+    private void shiftStacks() {
+        if (stackOldest.isEmpty()) {
+            while (!stackNewest.isEmpty()) {
+                stackOldest.push(stackNewest.pop());
+            }
+        }
+    }
 }
