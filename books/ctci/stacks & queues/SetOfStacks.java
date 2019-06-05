f3d873a26654ae70c89b95f5617126228f06fe31
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-06-05 05:59:27 +0200

    feat: add solution to stack of stacks

---
+++ books/ctci/stacks & queues/SetOfStacks.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class SetOfStacks {
+
+    private ArrayList<Stack> stacks = new ArrayList<>();
+    private int threshold;    
+
+    public SetOfStacks(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public void push(int value) {
+
+    }
+
+    public int pop() {
+        
+    }
+}
