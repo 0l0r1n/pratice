8f19127e0fb2b16f22a5b50928627857feb22185
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-06-05 06:01:25 +0200

    chore: clean up

--- books/ctci/stacks & queues/SetOfStacks.java
+++ books/ctci/stacks & queues/SetOfStacks.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Stack;
 
 public class SetOfStacks {
 
@@ -10,10 +12,43 @@ public class SetOfStacks {
     }
 
     public void push(int value) {
-
+        Stack last = getLastStack();
+        if (last != null && !last.isFull()) last.push(value);
+        else {
+            Stack stack = new Stack(this.threshold);
+            stack.push(value);
+            stacks.add(s);
+        }
     }
 
     public int pop() {
-        
+        Stack last = getLastStack();
+        if (last == null) throw new EmptyStackException();
+        int v = last.pop();
+        if (last.size() == 0) stacks.remove(stacks.size() - 1);
+        return v;
+    }
+
+    public int popAt(int index) {
+        return leftShift(index, true);
+    }
+
+    public int leftShift(int index, boolean removeTop) {
+        Stack stack = stacks.get(index);
+        int removedItem;
+        if (removeTop) removedItem = stack.pop();
+        else removedItem = stack.removeBottom();
+        if (stack.isEmpty) {
+            stacks.remove(index);
+        } else if (stacks.size() > index + 1) {
+            int v = leftShift(index + 1, false);
+            stack.push(v);
+        }
+        return removedItem;
+    }
+
+    private Stack getLastStack() {
+        if (stacks.size() == 0) return null;
+        return stacks.get(stacks.size() - 1);
     }
 }
