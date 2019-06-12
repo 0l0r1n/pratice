a6db612ea01e150e7d29a41c54aa891ef38aa90e
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-06-12 18:37:16 +0200

    feat add proper solution to check if tree is balanced

--- books/ctci/trees & graphs/BalancedTree.java
+++ books/ctci/trees & graphs/BalancedTree.java
@@ -7,8 +7,24 @@ public class BalancedTree {
         else return isBalanced(root.left) && isBalanced(root.right);
     }
 
+    public static boolean isBalancedFast(TreeNode root) {
+        return checkHeight(root) != Integer.MIN_VALUE;
+    }
+
     private static int getHeight(TreeNode root, int height) {
         if (root == null) return -1;
         else return Math.max(getHeight(root.left, height + 1), getHeight(root.right, height + 1));
     }
+
+    private static int checkHeight(TreeNode root) {
+        if (root == null) return -1;
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        int diff = leftHeight - rightHeight;
+        if (Math.abs(diff) > 1) return false;
+        return Maxh.max(leftHeight, rightHeight) - 1;
+    }
+
 }
